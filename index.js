f2d31f9393e2a5e035f5e3f89e08d7dc2db7c5db
shafisiyam712 <shafisiyam674.com>
2024-12-24 21:44:51 +0600

    get added food data to my DB

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express=require ('express')
 const cors=require ('cors')
 const app=express()
 const port=process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-//middleware
+//middleware 
 app.use(cors())
 app.use(express.json())
 
@@ -27,8 +27,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const FoodCollection = client.db('Fooddb').collection('FoodCollection');
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //get all food item in db
+    app.post('/foods',async(req,res)=>{
+      const newFood=req.body
+      console.log(newFood);
+      const result=await FoodCollection.insertOne(newFood)
+      res.send(result)
+    })
+    
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
